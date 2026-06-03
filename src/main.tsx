65ede81f1bba1a67485245f12175e88081f11c7f
Edgar <edgarmontenegro321@gmail.com>
2026-06-02 22:36:03 -0300

    ⚙️ feat: wrap application root with language and theme providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from './context/ThemeContext'
+import { LanguageProvider } from './context/LanguageContext'
+import App from './App'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <LanguageProvider>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </LanguageProvider>
+    </StrictMode>
 )
