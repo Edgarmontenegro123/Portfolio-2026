663b17a09faa8e19354e947daeb453f55e38bebf
Edgar <edgarmontenegro321@gmail.com>
2026-07-23 12:49:11 -0300

    ⚜️ feat(projects): add gordoni-web project to the list

--- src/constants/projectsData.ts
+++ src/constants/projectsData.ts
@@ -1,135 +1,93 @@
-import type { Project } from '../types/project'
-import LogoBasketStats from '../assets/Images/basketStatsLogo.png'
-import GifBasketStats from '../assets/Images/gifBasketStats.gif'
 import LogoTaskMateEvolution from '../assets/Images/taskMateEvolutionLogo.png'
+import LogoGordoniLtd from '../assets/Images/GordoniLtdLogo.png'
 import LogoQuizUp from '../assets/Images/quizUpLogo.png'
 import LogoClimApp from '../assets/Images/sunnyCloud.png'
 import LogoHealthier from '../assets/Images/healthier.png'
 import LogoTaskMate from '../assets/Images/tasksLogo.png'
 import LogoYoutube from '../assets/Images/youtubeLogo.png'
 import GifTaskMateEvolution from '../assets/Images/gifTaskMateEvolution.gif'
+import GifGordoniLtd from '../assets/Images/GifGordoniLtd.gif'
 import GifQuizUp from '../assets/Images/gifQuizUp.gif'
 import GifClimApp from '../assets/Images/gifClimApp.gif'
 import GifHealthier from '../assets/Images/gifHealthier.gif'
 import GifTaskMate from '../assets/Images/gifTaskMate.gif'
 import GifYoutubeChallenge from '../assets/Images/gifYoutubeChallenge.gif'
-/*gifBasketStats*/
-export const projectsMockUp: Project[] = [
-    {
-        id: 0,
-        avatar: LogoBasketStats,
-        title: 'Basket Stats',
-        date: {
-            es: 'Junio 2026',
-            'en-GB': 'June 2026'
-        },
-        img: GifBasketStats,
-        video: 'https://vimeo.com/1206319207',
-        description: {
-            es: 'Plataforma integral para la gestión y análisis de estadísticas de básquetbol. Incluye un panel de administración para el manejo de datos, APIs dedicadas para el procesamiento analítico y un frontend intuitivo para visualizar el rendimiento de los equipos y jugadores.',
-            'en-GB': 'Comprehensive platform for managing and analysing basketball statistics. It features an administration panel for data management, dedicated APIs for analytical processing, and an intuitive frontend to visualise team and player performance.'
-        },
-        tech: 'TypeScript, React, Node.js, Express, PostgreSQL',
-        deploy: 'https://basket-stats-frontend.vercel.app/',
-        gitHub: 'https://github.com/Edgarmontenegro123/basket-stats-frontend'
-    },
+
+const MockUp = [{
+    id: 0,
+    avatar: LogoTaskMateEvolution,
+    title: 'Task Mate Evolution!',
+    date: 'Octubre 2025',
+    img: GifTaskMateEvolution,
+    resume: 'Task Mate Evolution es un gestor de tareas y notas móvil que combina texto y voz en una interfaz moderna y fluida.',
+    video: 'https://vimeo.com/manage/videos/1135584450',
+    description: 'Task Mate Evolution es una aplicación móvil desarrollada con React Native (Expo) y TypeScript, diseñada como un gestor inteligente de tareas y notas, combinando texto y grabaciones de voz dentro de una interfaz moderna, minimalista y fluida. Próximamente en Google Play y AppStore',
+    tech: 'TypeScript, React Native, Expo SDH 54',
+    deploy: 'https://task-mate-by-montenegro.vercel.app/',
+    gitHub: 'https://github.com/Edgarmontenegro123/task-mate-evolution'
+},
     {
         id: 1,
-        avatar: LogoTaskMateEvolution,
-        title: 'Task Mate Evolution!',
-        date: {
-            es: 'Octubre 2025',
-            'en-GB': 'October 2025'
-        },
-        img: GifTaskMateEvolution,
-        video: 'https://vimeo.com/manage/videos/1135584450',
-        description: {
-            es: 'Task Mate Evolution es una aplicación móvil nativa desarrollada con React Native (Expo) y TypeScript, diseñada como un gestor inteligente ' +
-                'de tareas y notas de alto rendimiento. La plataforma revoluciona la productividad al integrar de forma fluida la carga de texto tradicional ' +
-                'con un avanzado sistema de grabaciones de voz y almacenamiento local persistente. Su arquitectura está optimizada con un enfoque "Offline-First", ' +
-                'garantizando un funcionamiento fluido, seguro y sin interrupciones incluso en entornos sin conectividad. Presenta una interfaz moderna, minimalista' +
-                ' y con transiciones de alta fidelidad, actualmente adaptada para su próximo despliegue en las tiendas oficiales de Google Play y Apple App Store.',
-            'en-GB': 'Task Mate Evolution is a native mobile application built with React Native (Expo) and TypeScript, designed as a high-performance, ' +
-                'intelligent task and note manager. The platform revolutionises productivity by seamlessly integrating traditional text input with an advanced ' +
-                'voice recording system and persistent local storage. Its architecture is fully optimised with an "Offline-First" approach, ensuring a fluid, ' +
-                'secure and uninterrupted user experience even in environments without network connectivity. Featuring a modern, minimalist interface with ' +
-                'high-fidelity transitions, the application is currently tailored for its upcoming deployment on the official Google Play and Apple App Store.'
-        },
-        tech: 'TypeScript, React Native, Expo SDK 54',
-        deploy: 'https://task-mate-by-montenegro.vercel.app/',
-        gitHub: 'https://github.com/Edgarmontenegro123/task-mate-evolution'
+        avatar: LogoGordoniLtd,
+        title: 'Gordoni Limited',
+        date: 'Abril 2026',
+        img: GifGordoniLtd,
+        resume: 'Sitio web corporativo desarrollado para establecer la presencia digital de Gordoni Limited con un diseño moderno, profesional y responsive.',
+        video: 'https://vimeo.com/1211913727?share=copy&fl=sv&fe=ci',
+        description: 'Gordoni Limited es un proyecto freelance desarrollado en tiempo récord para dotar a la empresa de una presencia digital sólida y profesional. La aplicación consiste en un sitio web corporativo moderno, completamente responsive y optimizado para ofrecer una navegación fluida en cualquier dispositivo. Construido con React, TypeScript, Vite y Tailwind CSS, el proyecto prioriza el rendimiento, la experiencia de usuario y una identidad visual alineada con la marca, permitiendo a la empresa presentar sus servicios de manera clara y generar confianza desde su lanzamiento.',
+        tech: 'React, TypeScript, Tailwind CSS, Vite',
+        deploy: 'https://www.gordoniltd.com/',
+        gitHub: 'https://github.com/Edgarmontenegro123/gordoni-web'
     },
     {
         id: 2,
         avatar: LogoQuizUp,
         title: 'QuizUp!',
-        date: {
-            es: 'Septiembre 2024',
-            'en-GB': 'September 2024'
-        },
+        date: 'Septiembre 2024',
         img: GifQuizUp,
+        resume: 'Juego de trivia que permite a los usuarios responder preguntas, escoger entre diferentes categorías y elegir la dificultad para jugar!',
         video: 'https://vimeo.com/manage/videos/1010081429',
-        description: {
-            es: 'Es un juego de trivia que permite a los usuarios responder preguntas, escoger entre muchísimas categorías y elegir la dificultad para jugar!',
-            'en-GB': 'It is a trivia game that allows users to answer questions, choose from a wide range of categories, and select the difficulty level to play!'
-        },        tech: 'TypeScript, React, NextJs, Tailwind, Swiper, ESLint, Prettier, Jest, React Testing Library',
+        description: 'Es un juego de trivia que permite a los usuarios responder preguntas, escoger entre muchísimas categorías y elegir la dificultad para jugar!',
+        tech: 'TypeScript, React, NextJs, Tailwind, Swiper, ESLint, Prettier, Jest, React Testing Library',
         deploy: 'https://quizup-by-montenegro.vercel.app/',
         gitHub: 'https://github.com/Edgarmontenegro123/quizup'
     },
     {
-        id: 5,
-        avatar: LogoHealthier,
-        title: 'Healthier',
-        date: {
-            es: 'Agosto 2021',
-            'en-GB': 'August 2021'
-        },
-        img: GifHealthier,
-        video: 'https://vimeo.com/manage/videos/586633960',
-        description: {
-            es: 'Con una interfaz muy intuitiva, que permite comprar productos saludables, ordenarlos por precio, por categoría, incluirlos en una wishlist o ' +
-                'llevarlos a un carrito de compra donde se permite terminar el proceso de compra mediante una pasarela de pagos.',
-            'en-GB': 'Featuring a highly intuitive interface that allows users to purchase healthy products, sort them by price or category, add them to a wishlist, ' +
-                'or place them in a shopping cart to complete the checkout process via a payment gateway.'
-        },        tech: 'React, Redux, Material UI, NodeJS, PostgreSQL',
-        deploy: 'https://Healthier.vercel.app',
-        gitHub: 'https://github.com/Healthier-Group/Healthier'
-    },
-    {
-        id: 4,
+        id: 3,
         avatar: LogoClimApp,
         title: 'ClimApp',
-        date: {
-            es: 'Marzo 2021',
-            'en-GB': 'March 2021'
-        },
+        date: 'Marzo 2021',
         img: GifClimApp,
+        resume: 'Primera App FrontEnd realizada con React y una API externa.',
         video: 'https://vimeo.com/manage/videos/586698718',
-        description: {
-            es: 'Esta es una App con una interfaz amigable, que muestra la hora y la fecha detallada del lugar donde te encuentres, con un buscador intuitivo, ' +
-                'para que elijas tus ciudades favoritas y en ellas puedas observar los datos climáticos más relevantes.',
-            'en-GB': 'This is an app with a user-friendly interface that displays the detailed time and date of your location, featuring an intuitive search ' +
-                'tool to select your favourite cities and view their most relevant weather data.'
-        },        tech: 'JavaScript, React, Material UI, HTML5, CSS3',
+        description: 'Esta es una App con una interfaz amigable, que muestra la hora y la fecha detallada del lugar donde te encuentres, con un buscador intuitivo, para que elijas tus ciudades favoritas y en ellas puedas observar los datos climáticos más relevantes.',
+        tech: 'JavaScript, React, Material UI, HTML5, CSS3',
         deploy: 'https://climappmontenegro.vercel.app',
         gitHub: 'https://github.com/Edgarmontenegro123/ClimApp'
     },
     {
-        id: 3,
+        id: 4,
+        avatar: LogoHealthier,
+        title: 'Healthier',
+        date: 'Agosto 2021',
+        img: GifHealthier,
+        resume: 'E-Commerce realizado de manera grupal como proyecto final para Soy Henry Bootcamp.',
+        video: 'https://vimeo.com/manage/videos/586633960',
+        description: 'Con una interfaz muy intuitiva, que permite comprar productos saludables, ordenarlos por precio, por categoría, incluirlos en una wishlist  o llevarlos a un carrito de compra donde se permite terminar el proceso de compra mediante una pasarela de pagos.',
+        tech: 'React, Redux, Material UI, NodeJS, PostgreSQL',
+        deploy: 'https://Healthier.vercel.app',
+        gitHub: 'https://github.com/Healthier-Group/Healthier'
+    },
+    {
+        id: 5,
         avatar: LogoTaskMate,
         title: 'Task Mate',
-        date: {
-            es: 'Septiembre 2024',
-            'en-GB': 'September 2024'
-        },
+        date: 'Septiembre 2024',
         img: GifTaskMate,
+        resume: 'Aplicación basada en el famoso proyecto To-Do List, potenciada con diferentes funcionalidades.',
         video: 'https://vimeo.com/manage/videos/1010457455',
-        description: {
-            es: 'Esta es una App con una interfaz amigable, que permite agregar notas o tareas, modificarlas, separarlas por colores, organizarlas con un ' +
-                'simple arrastre, eliminar las tareas completadas y guardarlas en tu dispositivo entre otras funcionalidades.',
-            'en-GB': 'This is an app with a friendly interface that lets you add notes or tasks, modify them, colour-code them, organise them with a ' +
-                'simple drag-and-drop, delete completed tasks, and save them on your device among other features.'
-        },        tech: 'Typescript, React, NextJs, Tailwind, HTML5, CSS3',
+        description: 'Esta es una App con una interfaz amigable, que permite agregar notas o tareas, modificarlas, separarlas por colores, organizarlas con un simple arrastre, eliminar las tareas completadas y guardarlas en tu dispositivo entre otras funcionalidades.',
+        tech: 'Typescript, React, NextJs, Tailwind, HTML5, CSS3',
         deploy: 'https://task-mate-by-montenegro.vercel.app/',
         gitHub: 'https://github.com/Edgarmontenegro123/task-mate'
     },
@@ -137,19 +95,14 @@ export const projectsMockUp: Project[] = [
         id: 6,
         avatar: LogoYoutube,
         title: 'Youtube-Challenge',
-        date: {
-            es: 'Noviembre 2023',
-            'en-GB': 'November 2023'
-        },
+        date: 'Noviembre 2023',
         img: GifYoutubeChallenge,
+        resume: 'App FrontEnd realizada con React y la API de Youtube.',
         video: 'https://vimeo.com/manage/videos/586698718',
-        description: {
-            es: 'Esta es una App con una interfaz amigable, que permite ingresar una URL de Youtube en un buscador intuitivo, que trae el título del video, la ' +
-                'cantidad de views y sus comentarios.',
-            'en-GB': 'This is an app with a friendly interface that allows users to enter a YouTube URL into an intuitive search bar, fetching the video title, ' +
-                'view count, and its comments.'
-        },        tech: 'JavaScript, React, Sass, HTML5, CSS3, Youtube API, Express',
+        description: 'Esta es una App con una interfaz amigable, que permite ingresar una URL de Youtube en un buscador intuitivo, que trae el título del video, la cantidad de views y sus comentarios.',
+        tech: 'JavaScript, React, Sass, HTML5, CSS3, Youtube API, Express',
         deploy: 'https://challenge-youtube.vercel.app/',
         gitHub: 'https://github.com/Edgarmontenegro123/challenge-youtube'
-    }
-]
+    }]
+
+export default MockUp
